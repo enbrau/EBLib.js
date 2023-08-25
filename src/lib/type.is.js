d5dbb9d949177b97c86318f10dcd4f57d647acd2
Enbrau <enbrau@gmail.com>
2023-08-25 14:49:46 +0800

    chore modify code annotation

--- src/lib/type.is.js
+++ src/lib/type.is.js
@@ -2,8 +2,10 @@
 
 /**
  * Checks if value is classified as a Number primitive or object.
+ * To exclude Infinity, -Infinity, and NaN, which are classified as numbers, use the isFiniteNumber method.
  * @param {*} x the value to check
  * @returns true if the value is number
+ * @since 0.0.1
  */
 export function isNumber(x) {
   return typeof x === 'number'
@@ -13,6 +15,7 @@ export function isNumber(x) {
  * Checks if value is a finite primitive number.
  * @param {*} x the value to check
  * @returns true if the value is finite number
+ * @since 0.0.1
  */
 export function isFiniteNumber(x) {
   return typeof x === 'number' && isFinite(x)
