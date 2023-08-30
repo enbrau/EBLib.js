553ae2c937bfae620f3fd069fc7e91b0538ffea3
Enbrau <enbrau@gmail.com>
2023-08-30 23:57:58 +0800

    feat add function

--- src/lib/random.js
+++ src/lib/random.js
@@ -7,3 +7,11 @@
 export function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
+
+/**
+ * Generate random serial number
+ * @returns random number
+ */
+export function serialNo() {
+  return parseInt(new Date().getTime() + '' + Math.floor(Math.random() * (999 - 100 + 1) + 100))
+}
