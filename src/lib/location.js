7d62405dbb01ab47bff298c70cdd0bc0ec47f2bd
Enbrau <enbrau@gmail.com>
2023-09-08 08:44:14 +0800

    feat add function

--- src/lib/location.js
+++ src/lib/location.js
@@ -77,3 +77,21 @@ export function wgs84ToBd09ll({ lng, lat }) {
     lat: bdlat
   }
 }
+
+function transformlat(lng, lat) {
+    const PI = 3.1415926535897932384626
+    let ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + 0.1 * lng * lat + 0.2 * Math.sqrt(Math.abs(lng))
+    ret += (20.0 * Math.sin(6.0 * lng * PI) + 20.0 * Math.sin(2.0 * lng * PI)) * 2.0 / 3.0
+    ret += (20.0 * Math.sin(lat * PI) + 40.0 * Math.sin(lat / 3.0 * PI)) * 2.0 / 3.0
+    ret += (160.0 * Math.sin(lat / 12.0 * PI) + 320 * Math.sin(lat * PI / 30.0)) * 2.0 / 3.0
+    return ret
+}
+
+function transformlng(lng, lat) {
+    const PI = 3.1415926535897932384626
+    let ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + 0.1 * lng * lat + 0.1 * Math.sqrt(Math.abs(lng))
+    ret += (20.0 * Math.sin(6.0 * lng * PI) + 20.0 * Math.sin(2.0 * lng * PI)) * 2.0 / 3.0
+    ret += (20.0 * Math.sin(lng * PI) + 40.0 * Math.sin(lng / 3.0 * PI)) * 2.0 / 3.0
+    ret += (150.0 * Math.sin(lng / 12.0 * PI) + 300.0 * Math.sin(lng / 30.0 * PI)) * 2.0 / 3.0
+    return ret
+}
