6fd0c8dcba4c9192a4e0b1103c8ed93498bc8311
Enbrau <enbrau@gmail.com>
2023-09-11 17:01:01 +0800

    chore refactor

--- src/lib/location.js
+++ src/lib/location.js
@@ -57,8 +57,21 @@ export function wgs84ToBd09ll({ lng, lat }) {
   const a = 6378245.0
   const ee = 0.00669342162296594323
 
-  let dlat = transformlat(lng - 105.0, lat - 35.0)
-  let dlng = transformlng(lng - 105.0, lat - 35.0)
+  let dlat = ((lng, lat) => {
+    let ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + 0.1 * lng * lat + 0.2 * Math.sqrt(Math.abs(lng))
+    ret += (20.0 * Math.sin(6.0 * lng * PI) + 20.0 * Math.sin(2.0 * lng * PI)) * 2.0 / 3.0
+    ret += (20.0 * Math.sin(lat * PI) + 40.0 * Math.sin(lat / 3.0 * PI)) * 2.0 / 3.0
+    ret += (160.0 * Math.sin(lat / 12.0 * PI) + 320 * Math.sin(lat * PI / 30.0)) * 2.0 / 3.0
+    return ret
+  }) (lng - 105.0, lat - 35.0)
+  let dlng = ((lng, lat) => {
+    let ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + 0.1 * lng * lat + 0.1 * Math.sqrt(Math.abs(lng))
+    ret += (20.0 * Math.sin(6.0 * lng * PI) + 20.0 * Math.sin(2.0 * lng * PI)) * 2.0 / 3.0
+    ret += (20.0 * Math.sin(lng * PI) + 40.0 * Math.sin(lng / 3.0 * PI)) * 2.0 / 3.0
+    ret += (150.0 * Math.sin(lng / 12.0 * PI) + 300.0 * Math.sin(lng / 30.0 * PI)) * 2.0 / 3.0
+    return ret
+  }) (lng - 105.0, lat - 35.0)
+  
   let radlat = lat / 180.0 * PI
   let magic = Math.sin(radlat)
   magic = 1 - ee * magic * magic
@@ -76,22 +89,4 @@ export function wgs84ToBd09ll({ lng, lat }) {
     lng: bdlng,
     lat: bdlat
   }
-}
-
-function transformlat(lng, lat) {
-    const PI = 3.1415926535897932384626
-    let ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + 0.1 * lng * lat + 0.2 * Math.sqrt(Math.abs(lng))
-    ret += (20.0 * Math.sin(6.0 * lng * PI) + 20.0 * Math.sin(2.0 * lng * PI)) * 2.0 / 3.0
-    ret += (20.0 * Math.sin(lat * PI) + 40.0 * Math.sin(lat / 3.0 * PI)) * 2.0 / 3.0
-    ret += (160.0 * Math.sin(lat / 12.0 * PI) + 320 * Math.sin(lat * PI / 30.0)) * 2.0 / 3.0
-    return ret
-}
-
-function transformlng(lng, lat) {
-    const PI = 3.1415926535897932384626
-    let ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + 0.1 * lng * lat + 0.1 * Math.sqrt(Math.abs(lng))
-    ret += (20.0 * Math.sin(6.0 * lng * PI) + 20.0 * Math.sin(2.0 * lng * PI)) * 2.0 / 3.0
-    ret += (20.0 * Math.sin(lng * PI) + 40.0 * Math.sin(lng / 3.0 * PI)) * 2.0 / 3.0
-    ret += (150.0 * Math.sin(lng / 12.0 * PI) + 300.0 * Math.sin(lng / 30.0 * PI)) * 2.0 / 3.0
-    return ret
 }
