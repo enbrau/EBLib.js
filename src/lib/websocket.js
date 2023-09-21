e7fd0e863350002018a5ba8f760a804ffd21e4fd
Enbrau <enbrau@gmail.com>
2023-09-21 09:04:58 +0800

    chore add function

--- src/lib/websocket.js
+++ src/lib/websocket.js
@@ -2,15 +2,79 @@
  * Wrapped WebSocket
  * @returns Wrapped WebSocket
  */
-function WS() {
+function WS(opts) {
   this.webSocket = null
+
+  this.open = function(opts) {
+    if (!opts.url) {
+      return
+    }
+
+    // Handle URL
+    let url = opts.url
+    if (url.startsWith('http://')) {
+      url.replace('http',  'ws')
+    } else if (url.startsWith('https://')) {
+      url.replace('https', 'wss')
+    } else if (!url.startsWith('ws')) {
+      const schema = opts.schema || 'ws://'
+      url = schema + url
+    }
+
+    // Create Connection
+    try {
+      this.webSocket = new WebSocket(url)
+      this.bind(opts)
+      this.keepAlive(opts)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  this.bind = function(opts) {
+    if (!this.webSocket) {
+      return
+    }
+
+    this.webSocket.onmessage = opts.onmessage || function(e) {
+      // TODO
+    }
+
+    this.webSocket.onerror = opts.onerror || function(e) {
+      // TODO
+    }
+
+    this.webSocket.onclose = opts.onclose || function(e) {
+      // TODO
+    }
+  }
+
+  const queue = []
+  this.send = function(msg) {
+    if (!this.webSocket) {
+      return
+    }
+
+    if (this.webSocket.readyState != WebSocket.OPEN) {
+      queue.push(msg)
+      return
+    }
+
+    while (queue.length > 0) {
+      const qmsg = queue.shift()
+      this.webSocket.send(qmsg)
+    }
+    this.webSocket.send(msg)
+  }
+
   // TODO
 
 
   this.timeout = 20000
   this.timerID = 0
 
-  this.keepAlive = function() {
+  this.keepAlive = function(opts) {
+    this.timeout = opts && opts.timeout ? opts.timeout : this.timeout
     if (this.webSocket && this.webSocket.readyState == WebSocket.OPEN) {
       this.webSocket.send('')
     }
@@ -22,6 +86,8 @@ function WS() {
       clearTimeout(this.timerID)
     }
   }
+
+  this.open(opts)
 
   return this
 }
