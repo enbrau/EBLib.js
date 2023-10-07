a78df44640ea34e03a54163fc825522bf37d446f
Enbrau <enbrau@gmail.com>
2023-10-07 08:57:59 +0800

    feat add function

--- src/lib/formatter.js
+++ src/lib/formatter.js
@@ -87,3 +87,43 @@ export function camelCaseToKebabCase(str) {
 export const kebabCaseToCamelCase = (str) => {
   return str.replace( /-([a-z])/g, (i, item) => item.toUpperCase())
 }
+
+/**
+ * 
+ * @param {*} str 
+ * @returns 
+ */
+export const toCDB = (str) => {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    code = str.charCodeAt(i);
+    if (code >= 65281 && code <= 65374) {
+      result += String.fromCharCode(str.charCodeAt(i) - 65248);
+    } else if (code == 12288) {
+      result += String.fromCharCode(str.charCodeAt(i) - 12288 + 32);
+    } else {
+      result += str.charAt(i);
+    }
+  }
+  return result;
+}
+
+/**
+ * 
+ * @param {*} str 
+ * @returns 
+ */
+export const toDBC = (str) => {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    code = str.charCodeAt(i);
+    if (code >= 33 && code <= 126) {
+      result += String.fromCharCode(str.charCodeAt(i) + 65248);
+    } else if (code == 32) {
+      result += String.fromCharCode(str.charCodeAt(i) + 12288 - 32);
+    } else {
+      result += str.charAt(i);
+    }
+  }
+  return result;
+}
