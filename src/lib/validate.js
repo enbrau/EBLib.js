f2e900927735dd566e1fc0b5bd998b914e6b622b
Enbrau <enbrau@gmail.com>
2023-10-10 14:07:06 +0800

    feat add function

--- src/lib/validate.js
+++ src/lib/validate.js
@@ -47,3 +47,12 @@ export function containsEmoji(value) {
 export function isTel(value) {
   return /^1[3,4,5,6,7,8,9][0-9]{9}$/.test(value.toString())
 }
+
+/**
+ * 
+ * @param {*} value 
+ * @returns 
+ */
+export function containsChinese(value) {
+  return /[\u4e00-\u9fa5]/.test(value);
+}
