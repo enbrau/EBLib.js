a73e5125f352c2df097df6dfe07d10c59752e524
Enbrau <enbrau@gmail.com>
2023-10-24 09:03:31 +0800

    feat add function

--- src/lib/browser.js
+++ src/lib/browser.js
@@ -200,3 +200,18 @@ export function toFullScreen() {
     element.webkitRequestFullScreen()
   }
 }
+
+/**
+ * Exit Full Screen
+ */
+export function exitFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen()
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen()
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen()
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen()
+  }
+}
